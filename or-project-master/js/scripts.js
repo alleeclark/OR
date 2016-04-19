4f4797d92382814d521d7eda965d702228bcdb33
alleeclark <allee.clark@bison.howard.edu>
2016-04-18 21:15:50 -0400

    updated store in

--- or-project-master/js/scripts.js
+++ or-project-master/js/scripts.js
@@ -1,13 +1,14 @@
 var reader; //GLOBAL File Reader object for demo purpose only
 var dataSum = 0;
 var dataSet = [];
+var coordinates = [{}];
 /**
     * Check for the various File API support.
  */
 function checkFileAPI() {
     if (window.File && window.FileReader && window.FileList && window.Blob) {
-    reader = new FileReader();
-    return true; 
+        reader = new FileReader();
+        return true;
     } else {
         alert('The File APIs are not fully supported by your browser. Fallback required.');
         return false;
@@ -20,38 +21,42 @@ function readText(filePath) {
     var output = ""; //placeholder for text output
     var stringArray = new Array();
     var yVals = new Array();
-    if(filePath.files && filePath.files[0]) {           
+    if (filePath.files && filePath.files[0]) {
         reader.onload = function (e) {
             output = e.target.result;
-            cleanOutput = output.replace(/\s+/g,"").replace(/\./g," ").replace(/\s+$/, ""); //remove unnecessary whitespace, period, and then remove space from end of string
+            cleanOutput = output.replace(/\s+/g, "").replace(/\./g, " ").replace(/\s+$/, ""); //remove unnecessary whitespace, period, and then remove space from end of string
             stringArray = cleanOutput.split(" ");
             stringArray.pop();
             //for 100% of data
-            for(i = 0; i < stringArray.length; i++) { //loop through array, convert each element to an integer, then add to yVals
+            for (i = 0; i < stringArray.length; i++) { //loop through array, convert each element to an integer, then add to yVals
                 x = parseInt(stringArray[i], 10); //convert each number
                 dataSum += x;
                 yVals[i] = x;
             }
-            document.getElementById("full-data-graph").innerHTML = "";
             populateDataset(dataSum, yVals);
-            scatterChart(dataSet, 100);
+            if (document.getElementById('scatter').checked) {
+                document.getElementById("full-data-graph").innerHTML = "";
+                scatterChart(dataSet, 100);
+            }
             //for 30% of data
             dataSum = 0; //reset data sum for new data
             dataSet.length = 0; //reset dataSet array for new data
             yVals.length = 0; //reset yVals array for new data
             limit = Math.floor(stringArray.length * .3); //set limit to 30% of values from file data
-            for(i = 0; i < limit; i++) { //loop through array, convert each element to an integer, then add to yVals
+            for (i = 0; i < limit; i++) { //loop through array, convert each element to an integer, then add to yVals
                 x = parseInt(stringArray[i], 10); //convert each number
                 dataSum += x;
                 yVals[i] = x;
             }
-            document.getElementById("partial-data-graph").innerHTML = "";
             populateDataset(dataSum, yVals);
-            scatterChart(dataSet, 30);
+            if (document.getElementById('scatter').checked) {
+                document.getElementById("partial-data-graph").innerHTML = "";
+                scatterChart(dataSet, 30);
+            }
         };//end onload()
         reader.readAsText(filePath.files[0]);
     }//end if html5 filelist support
-    else if(ActiveXObject && filePath) { //fallback to IE 6-8 support via ActiveX
+    else if (ActiveXObject && filePath) { //fallback to IE 6-8 support via ActiveX
         try {
             reader = new ActiveXObject("Scripting.FileSystemObject");
             var file = reader.OpenTextFile(filePath, 1); //ActiveX File Object
@@ -60,25 +65,25 @@ function readText(filePath) {
             console.log(cleanOutput);
             file.Close(); //close file "input stream"
             //displayContents(output);
-         }
+        }
          catch (e) {
             if (e.number == -2146827859) {
                 alert('Unable to access local files due to browser security settings. ' + 
                     'To overcome this, go to Tools->Internet Options->Security->Custom Level. ' + 
-                    'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"'); 
+                    'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"');
             }
-         }       
+        }
     }
     else { //this is where you could fallback to Java Applet, Flash or similar
         return false;
-    }       
+    }
     return true;
-}   
+}
 /**
     * display content using a basic HTML replacement
 */
 function displayContents(txt) {
-    var el = document.getElementById('main'); 
+    var el = document.getElementById('main');
     el.innerHTML = txt; //display output in DOM
 }
 /**
@@ -86,87 +91,88 @@ function displayContents(txt) {
     * add X and Y values to new array, which will be used to create graphs
 */
 function populateDataset(sum, array) {
-    for(i = 0; i < array.length; i++) {
+    xArray = [];
+    for (i = 0; i < array.length; i++) {
         xVal = array[i] / sum;
-        dataSet[i] = [xVal, array[i]];
+        xArray.push(xVal);
     }
-    return true;
+    coordinates = [{
+            xCoordinates: xArray,
+            yCoordinates: array
+        }];
+    //console.log("coordinates object (y coordinates array): " + coordinates.yCoordinates);
+    console.log(coordinates[0].xCoordinates);
+    console.log(coordinates[0].yCoordinates);
 }
 function scatterChart(dataset, percentage) {
-    //Chart dimensions
-            var width = 800;
-            var height = 600;            var padding = 30;
-
-            //Static test data
-            // var dataset = [
-            //                 [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-            //                 [410, 12], [475, 44], [25, 67], [85, 21], [220, 88],
-            //                 [600, 150]
-            // ];
-    //Clear divs with data (if there's anything in them)
-    //Create scales for the data (domain and range)
-        var xScale = d3.scale.linear()
-                             .domain([0, d3.max(dataset, function(d) { return d[0]; })]).nice() //get max value from dataset x-range
-                             .range([padding, width- padding * 2]);
-
-        var yScale = d3.scale.linear()
-                             .domain([0, d3.max(dataset, function(d) { return d[1]; })]) //get max value from dataset y-range
-                             .range([height - padding, padding]);
-
-        var rScale = d3.scale.linear()
-                             .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-                             .range([2, 5]);
-
-        var formatAsPercentage = d3.format(".1%");
-
-        //Define X axis
-        var xAxis = d3.svg.axis()
-                          .scale(xScale)
-                          .orient("bottom")
-                          .ticks(5)
-                          .tickFormat(formatAsPercentage);
-
-        //Define Y axis
-        var yAxis = d3.svg.axis()
-                          .scale(yScale)
-                          .orient("left")
-                          .ticks(5);
-    //Create SVG element in HTML page
-        if(percentage == 100 || percentage == 1) {
-            var svg = d3.select("#full-data-graph")
-                        .append("svg")
-                        .attr("width", width)
-                        .attr("height", height);
-        }
-        if(percentage == 30 || percentage == .3) {
-            var svg = d3.select("#partial-data-graph")
-                        .append("svg")
-                        .attr("width", width)
-                        .attr("height", height);
-        }
-            //Create points
-            svg.selectAll("circle")
-               .data(dataset)
-               .enter()
-               .append("circle")
-               .attr("cx", function(d) {
-                    return xScale(d[0]);
-               })
-               .attr("cy", function(d) {
-                    return yScale(d[1]);
-               })
-               .attr("r", function(d){
-                    return rScale(d[1]);
-               });
-            //Create X axis
-            svg.append("g")
-                .attr("class", "axis")
-                .attr("transform", "translate(0," + (height - padding) + ")")
-                .call(xAxis);
-            
-            //Create Y axis
-            svg.append("g")
-                .attr("class", "axis")
-                .attr("transform", "translate(" + padding + ",0)")
-                .call(yAxis);
+    var margin = {
+        top: 20,
+        right: 20,
+        bottom: 30,
+        left: 40
+    },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+    //setup x  and y scales I've used linear here but there are other options
+    // the scales translate data values to pixel values for you
+    var x = d3.scale.linear()
+              .domain([0, d3.max(coordinates[0].xCoordinates)])//the range of x values to plot
+              .range([0, width]);   //the pixel range of the x axis
+    var y = d3.scale.linear()
+              .domain([0, d3.max(coordinates[0].yCoordinates)])//the range of y values to plot
+              .range([height, 0]);
+    
+    //the chart object (placing the chart on the page)
+    if (percentage == 100 || percentage == 1) {
+        var scatterChart = d3.select('#full-data-graph')
+        .append('svg:svg')//append svg to svg element in html
+        .attr('width', width + margin.right + margin.left)//adjusts width to margins (given in margin Object above)
+        .attr('height', height + margin.top + margin.bottom)//adjusts height to margins (given in margin Object above)
+        .attr('class', 'chart')
+    }
+    //the chart object (placing the chart on the page)
+    if (percentage == 30 || percentage == .3) {
+        var scatterChart = d3.select('#full-data-graph')
+        .append('svg:svg')//append svg to svg element in html
+        .attr('width', width + margin.right + margin.left)//adjusts width to margins (given in margin Object above)
+        .attr('height', height + margin.top + margin.bottom)//adjusts height to margins (given in margin Object above)
+        .attr('class', 'chart')
+    }
+    // the main object where the chart and axis will be drawn
+    var main = scatterChart.append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('class', 'main')
+    
+    // draw the x axis
+    var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom');
+    
+    main.append('g')
+    .attr('transform', 'translate(0,' + height + ')')
+    .attr('class', 'main axis date')
+    .call(xAxis);
+    
+    // draw the y axis
+    var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient('left');
+    
+    main.append('g')
+    .attr('transform', 'translate(0,0)')
+    .attr('class', 'main axis date')
+    .call(yAxis);
+    
+    // draw the graph object
+    var g = main.append("svg:g");
+    
+    g.selectAll("scatter-dots")
+      .data(coordinates[0].yCoordinates)// using the values in the ydata array
+      .enter().append("svg:circle")// create a new circle for each value
+          .attr("cy", function (d) { return y(d); })// translate y value to a pixel
+          .attr("cx", function (d, i) { return x(coordinates[0].xCoordinates[i]); })// translate x value
+          .attr("r", 3)// radius of circle
+          .style("opacity", 0.6); // opacity of circle
 }
